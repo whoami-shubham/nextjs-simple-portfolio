32826fbebe8ebb4e23ca73c6b874c7a3d202d516
whoami-shubham <shubhamkumarjha0013@gmail.com>
2022-09-04 06:23:02 +0000

    whoami-shubham published a site update

---
+++ _next/static/jy0E1dyAfXSh_dv0Kt_hR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f2cbad34d1084f6a.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
