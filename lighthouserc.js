8eb6a698f1c9f3e29866c8a2a93b8412bae4087b
Shubham Jha <Shubhamkumarjha0013@gmail.com>
2022-09-14 22:20:57 +0530

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,18 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:3000/'],
-      startServerCommand: 'cd out && serve',
+      url: ["http://localhost:3000/"],
+      startServerCommand: "yarn start",
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+    assert: {
+      preset: "lighthouse:recommended",
+      assertions: {
+        canonical: "off",
+        "unused-javascript": "off",
+      },
     },
   },
-};
+}
